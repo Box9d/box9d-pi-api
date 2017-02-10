1591a27c3f8cfb7361e0648cdab8f267aba305ba
Rick <rickpowell1311@gmail.com>
2017-02-10 22:19:53 +0000

    Initial solution structure for API

--- src/app/output.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-router.get('/', function (req, res) {
-  res.send('<html><head></head><body><h1>Output</h1></body></html>')
-})
-
-module.exports = router
