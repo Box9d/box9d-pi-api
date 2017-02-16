0d8cefecb3e37d9e4ef49ba02cb6dc81bdb2c676
Rick <rickpowell1311@gmail.com>
2017-02-16 22:11:34 +0000

    Add Values controller and specify port for hosting using Kestrel

---
+++ src/Box9.Leds.Pi.Api/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Box9.Leds.Pi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController
+    {
+        [HttpGet]
+        public GlobalJsonResult<IEnumerable<string>> GetAll()
+        {
+            var result = new[] { "1", "2" };
+
+            return GlobalJsonResult<IEnumerable<string>>.Success(System.Net.HttpStatusCode.OK, result);
+        }
+    }
+}
--- src/Box9.Leds.Pi.Api/Program.cs
+++ src/Box9.Leds.Pi.Api/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Box9.Leds.Pi.Api
 {
@@ -8,8 +9,14 @@ namespace Box9.Leds.Pi.Api
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
