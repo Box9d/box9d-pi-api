598945518a659dade4cd8579bceb23b2dd6f267a
Rick Powell <richard.powell@sfwltd.co.uk>
2017-03-06 17:51:06 +0000

    Add Mono Service Host for running on Pi with mono-service

---
+++ src/Box9.Leds.Pi.MonoServiceHost/WebApiService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceProcess;
+using Box9.Leds.Pi.Api;
+using Microsoft.Owin.Hosting;
+
+namespace Box9.Leds.Pi.MonoServiceHost
+{
+    public partial class WebApiService : ServiceBase
+    {
+        private IDisposable webApp;
+
+        public WebApiService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string baseUrl = ConfigurationManager.AppSettings["Host"];
+            webApp = WebApp.Start<Startup>(baseUrl);
+        }
+
+        protected override void OnStop()
+        {
+            if (webApp != null)
+            {
+                webApp.Dispose();
+            }
+        }
+    }
+}
