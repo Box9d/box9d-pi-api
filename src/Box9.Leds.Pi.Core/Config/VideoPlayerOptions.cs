7ed6b735db49c40f4e900036c959cb228259a37f
Rick <rickpowell1311@gmail.com>
2017-03-25 13:30:09 +0000

    Update configuration for VideoPlayerOptions

--- src/Box9.Leds.Pi.Core/Config/VideoPlayerOptions.cs
+++ src/Box9.Leds.Pi.Core/Config/VideoPlayerOptions.cs
@@ -1,7 +1,15 @@
-﻿namespace Box9.Leds.Pi.Core.Config
+﻿using System.Configuration;
+
+namespace Box9.Leds.Pi.Core.Config
 {
     public class VideoPlayerOptions
-    { 
+    {
+        public VideoPlayerOptions()
+        {
+            UseFadecandyServer = bool.Parse(ConfigurationManager.AppSettings["UseFadecandyServer"]);
+            FadecandyPort = 7890;
+        }
+
         public bool UseFadecandyServer { get; set; }
 
         public int FadecandyPort { get; set; }
