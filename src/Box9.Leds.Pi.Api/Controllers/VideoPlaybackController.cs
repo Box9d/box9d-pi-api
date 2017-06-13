a1cd765aa8b5a75a617d9c177251262f5f9ace14
Rick Powell <richard.powell@sfwltd.co.uk>
2017-06-13 18:12:57 +0100

    Restore playback functionality

---
+++ src/Box9.Leds.Pi.Api/Controllers/VideoPlaybackController.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Web.Http;
+using Box9.Leds.Pi.Api.ApiRequests;
+using Box9.Leds.Pi.Domain.Videos;
+using Autofac;
+using System.Collections.Generic;
+using Box9.Leds.Pi.Domain.Logging;
+using Box9.Leds.Pi.Domain.VideoPlayback;
+
+namespace Box9.Leds.Pi.Api.Controllers
+{
+    public class VideoPlaybackController : ApiController
+    {
+        private readonly IVideoComponentService videoComponentService;
+        private readonly ILog log;
+        private readonly VideoPlayer videoPlayer;
+
+        public VideoPlaybackController()
+        {
+            videoComponentService = Startup.Container.Resolve<IVideoComponentService>();
+            log = Startup.Container.Resolve<ILog>();
+
+            this.videoPlayer = new VideoPlayer();
+        }
+
+        public VideoPlaybackController(IVideoComponentService videoComponentService, ILog log)
+        {
+            this.videoComponentService = videoComponentService;
+            this.log = log;
+
+            this.videoPlayer = new VideoPlayer();
+        }
+
+        [ActionName("Load")]
+
+        [HttpGet]
+        public GlobalJsonResult<EmptyResult> Load(int videoId, [FromBody]LoadVideoPlaybackRequest request)
+        {
+            var video = videoComponentService.GetById(videoId);
+            videoPlayer.Load(video);
+
+            return GlobalJsonResult<EmptyResult>.Success(HttpStatusCode.OK);
+        }
+
+        [ActionName("Play")]
+        [HttpPost]
+        public GlobalJsonResult<EmptyResult> Play(int videoId, [FromBody]PlayVideoRequest request)
+        {
+            videoPlayer.Play(request.TimeReferenceUrl, request.PlayAt);
+
+            return GlobalJsonResult<EmptyResult>.Success(HttpStatusCode.OK);
+        }
+
+        [ActionName("Stop")]
+        [HttpPost]
+        public GlobalJsonResult<EmptyResult> Stop(int videoId, [FromBody]StopVideoRequest request)
+        {
+            videoPlayer.Stop();
+
+            return GlobalJsonResult<EmptyResult>.Success(HttpStatusCode.OK);
+        }
+
+        [ActionName("Log")]
+        [HttpGet]
+        public GlobalJsonResult<IEnumerable<string>> Log()
+        {
+            return GlobalJsonResult<IEnumerable<string>>.Success(HttpStatusCode.OK, log.Messages);
+        }
+    }
+}
---
+++ src/Box9.Leds.Pi.Domain/VideoPlayback/VideoPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Box9.Leds.Pi.Domain.Logging;
+using Box9.Leds.WebSocket.ApiClient;
+using System.IO;
+
+namespace Box9.Leds.Pi.Domain.VideoPlayback
+{
+    public class VideoPlayer
+    {
+        private readonly ILog log;
+        private readonly WebSocketApiClient websocketClient;
+
+        public VideoPlayer()
+        {
+            this.websocketClient = new WebSocketApiClient(new Uri("http://localhost:8003"));
+        }
+
+        public void Load(Video video)
+        {
+            var sqliteDatabasePath = Path.Combine(Directory.GetCurrentDirectory(), "box9database.sqlite");
+            websocketClient.Load(new LoadRequest
+            {
+                DatabasePath = sqliteDatabasePath,
+                FrameRate = video.FrameRate,
+                VideoId = video.Id
+            }).Wait();
+        }
+
+        public async Task Play(string timeReferenceUrl, DateTime? playAt)
+        {
+            await websocketClient.Play(timeReferenceUrl, playAt);
+        }
+
+        public async Task Stop()
+        {
+            await websocketClient.Stop();
+        }
+    }
+}
